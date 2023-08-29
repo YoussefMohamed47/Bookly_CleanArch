0b0b04702ccc10e52cf1bdfdc3fcb16180f46240
yomohamed47 <101569961+yomohamed47@users.noreply.github.com>
2023-08-29 16:53:51 +0300

    adding failure class

--- lib/Features/Home/domain/repos/home_repo.dart
+++ lib/Features/Home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/Core/errors/failure.dart';
 import 'package:bookly/Features/Home/domain/enitities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBook();
-  Future<List<BookEntity>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBook();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsetBooks();
 }
